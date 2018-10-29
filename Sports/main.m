1ba73818933cfe0be4bca394f26bf4cff1d4b3c2
SpencerSymington <theSpencerSymington@gmail.com>
2018-10-29 11:10:30 -0230

    Initial Commit

---
+++ Sports/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sports
+//
+//  Created by Spencer Symington on 2018-10-29.
+//  Copyright © 2018 Spencer Symington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
